8b35fe07443b7b09ec1bec2214a808a448778854
Lushnykov Pavlo <lushnikov.vasa@gmail.com>
2022-11-08 10:57:20 +0200

    lab4 header and subHeader finished

--- lab 4/blog/src/components/home_page.tsx
+++ lab 4/blog/src/components/home_page.tsx
@@ -1,3 +1,4 @@
+import { search } from "../constants/pages_data";
 import { HomePageSubHeader } from "./home_page_subheader";
 import { Navbar } from "./navbar";
 
@@ -7,6 +8,12 @@ export function HomePage(){
         <div>
             <Navbar></Navbar>
             <HomePageSubHeader></HomePageSubHeader>
+            <div className="w-full pt-16 px-4 lg:py-24 lg:px-28">
+                <div className="flex flex-col lg:flex-row lg:justify-between">
+                    <input type="text" className="w-full lg:w-[280px] h-11 rounded-lg border-[#D0D5DD] border-[1px] p-[14px]" placeholder={search}/>
+                    <input type="text" className="w-full lg:w-[280px] h-11 rounded-lg border-[#D0D5DD] border-[1px] p-[14px] mt-4 lg:mt-0"/>
+                </div>
+            </div>
         </div>
     )
 }
--- lab 4/blog/src/constants/pages_data.tsx
+++ lab 4/blog/src/constants/pages_data.tsx
@@ -1,2 +1,17 @@
 export const webPageLogo = {title: "Untitled UI", icon: require("../assets/icons/main_header_icon.png")}
-export const burgerMenuStates = {general: require("../assets/icons/burger_menu.png"), active: require("../assets/icons/burger_menu_active.png")}
+
+export const headerLinks = [{title: "Home", link: "/"},
+                            {title: "Products", subLinks: [{title: "prodSub1", link: "#"}, {title: "prodSub2", link: "#"}]},
+                            {title: "Resources", subLinks: [{title: "resSub1", link: "#"}, {title: "resSub2", link: "#"}]},
+                            {title: "Pricing", link: "#"}]
+
+export const logIn = "Log in"
+export const signUp = "Sign up"
+
+export const homePageSubHeaderTextData = {subTitle: "Resources", title: "Untitled blog", 
+description: "Tool and strategies modern teams need to help their companies grow.",
+privacyPolicyNotice: "We care about you data in our",
+provacyPolicyLink: <a href="#" className=" underline">privacy policy</a>,
+buttonTitle: "Get started"}
+
+export const search = "Search"
